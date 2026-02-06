72b232785441ff5d853445ade344a0d675bf33a3
luanfelixcoding <luanfpcoding@gmail.com>
2026-02-05 23:13:57 -0300

    feat(sql): implement sql basics simple join with count

---
+++ SQL/rank-7kyu (Easy)/simple-join-with-count.sql
@@ -0,0 +1,13 @@
+/*
+Codewars Challenge: SQL Basics: Simple JOIN with COUNT
+Rank: 8 kyu
+URL: https://www.codewars.com/kata/580918e24a85b05ad000010c
+*/
+
+-- Create your SELECT statement here
+select p.id,
+       p.name,
+      COUNT(t.people_id) as "toy_count"
+from people p
+left join toys t on t.people_id = p.id
+group by p.id, p.name;
